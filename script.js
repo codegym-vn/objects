8cd63b4aa2ded131f8de203a2144fd23447608b3
KhacNhat <khacnhat.info@gmail.com>
2017-04-26 16:19:57 +0700

    First commit

---
+++ script.js
@@ -0,0 +1,35 @@
+/**
+ * Created by nhatnk on 4/26/17.
+ */
+
+function Hero(image, top, left, size){
+  this.image = image;
+  this.top = top;
+  this.left = left;
+  this.size = size;
+
+  this.getHeroElement = function(){
+    return '<img width="'+ this.size + '"' +
+      ' height="'+ this.size + '"' +
+      ' src="' + this.image +'"' +
+      ' style="top: '+this.top+'px; left:'+this.left+'px;position:absolute;" />';
+  }
+
+  this.moveRight = function(){
+    this.left += 20;
+    console.log('ok: ' + this.left);
+  }
+
+}
+
+var hero = new Hero('pikachu.png', 20, 30, 200);
+
+function start(){
+  if(hero.left < window.innerWidth - hero.size){
+    hero.moveRight();
+  }
+  document.getElementById('game').innerHTML = hero.getHeroElement();
+  setTimeout(start, 500)
+}
+
+start();
